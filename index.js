00363c757b370ce654a00c78d750922f74065c84
Salma Ahmed <salmahamzaahmed77@gmail.com>
2023-10-27 09:05:00 +0200

    server-side rendered-pages

--- index.js
+++ index.js
@@ -7,15 +7,23 @@ app.get("/", (req, res) => {
   res.send("Hello, NodeJs users!");
 });
 
+
+//Server side rendered pages
 app.get("/hello", (req, res) => {
-  res.send("Hello, world!");
+  res.sendFile(__dirname + "/views/hello.html");
 });
+
+app.get("/welcome", (req, res) => {
+    res.render("welcome.ejs", { name: req.query.name , age: req.query.age});
+  });
+
 
 app.post("/addComment", (req, res) => {
   res.send("POST on addComment");
 });
 
 
+//Client side rendered pages
 app.get("/findSummation/:n/:m", (req, res) => {
   const n = req.params.n;
   const m = req.params.m;
@@ -29,12 +37,12 @@ app.get("/findSummation/:n/:m", (req, res) => {
 app.get("/sayHelloTo", (req, res) => {
   const name = req.body.name;
   console.log(req.body);
-
-  // Send the response back to the client
   res.send(
     `Hello, ${name}. Your name has ${name.length} letters. And your Age should be ${req.query.age} years old.`
   );
 });
+
+
 
 app.listen(8000, () => {
   console.log("I'm listening in port 8000");

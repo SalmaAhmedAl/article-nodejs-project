3f76034c848942aadf2cbb2754fdcd1ca25557bc
Salma Ahmed <salmahamzaahmed77@gmail.com>
2023-10-27 08:17:07 +0200

    get

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res)=>{
     res.send("Hello, Express!");
@@ -17,6 +18,25 @@ app.post("/addComment", (req, res)=>{
 app.delete("/testingDelete", (req, res)=>{
     console.log("Delete request");
 });
+
+app.get("/findSummation/:n/:m", (req, res) => {
+    const n = req.params.n;
+    const m = req.params.m;
+    const total = Number(n) + Number(m);
+    console.log(`The sum of ${n} and ${m} is ${total}.`);
+    
+    // Send the response back to the client
+    res.send(`The sum of ${n} and ${m} is ${total}.`);
+  });
+
+  app.get("/sayHelloTo", (req, res) => {
+    const name = req.body.name;
+    console.log(req.body);
+    
+    // Send the response back to the client
+    res.send(`Hello, ${name}.`);
+  });
+
 
 app.listen(8000, ()=>{
     console.log("I'm listening in port 8000");

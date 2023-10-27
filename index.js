cb0b52448c7cb87bc4067e3e73223ff9de175081
Salma Ahmed <salmahamzaahmed77@gmail.com>
2023-10-27 08:25:52 +0200

    json response

--- index.js
+++ index.js
@@ -3,41 +3,39 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
-app.get("/", (req, res)=>{
-    res.send("Hello, Express!");
+app.get("/", (req, res) => {
+  res.send("Hello, NodeJs users!");
 });
 
-app.get("/hello", (req, res)=>{
-    res.send("Hello, world!");
+app.get("/hello", (req, res) => {
+  res.send("Hello, world!");
 });
 
-app.post("/addComment", (req, res)=>{
-    res.send("POST on addComment");
+app.post("/addComment", (req, res) => {
+  res.send("POST on addComment");
 });
 
-app.delete("/testingDelete", (req, res)=>{
-    console.log("Delete request");
-});
 
 app.get("/findSummation/:n/:m", (req, res) => {
-    const n = req.params.n;
-    const m = req.params.m;
-    const total = Number(n) + Number(m);
-    console.log(`The sum of ${n} and ${m} is ${total}.`);
-    
-    // Send the response back to the client
-    res.send(`The sum of ${n} and ${m} is ${total}.`);
-  });
+  const n = req.params.n;
+  const m = req.params.m;
+  const total = Number(n) + Number(m);
+  console.log(`The sum of ${n} and ${m} is ${total}.`);
 
-  app.get("/sayHelloTo", (req, res) => {
-    const name = req.body.name;
-    console.log(req.body);
-    
-    // Send the response back to the client
-    res.send(`Hello, ${name}.`);
-  });
+  // Send the response back to the client as a json
+  res.json({ num1: n, num2: m, total: total });
+});
 
+app.get("/sayHelloTo", (req, res) => {
+  const name = req.body.name;
+  console.log(req.body);
 
-app.listen(8000, ()=>{
-    console.log("I'm listening in port 8000");
+  // Send the response back to the client
+  res.send(
+    `Hello, ${name}. Your name has ${name.length} letters. And your Age should be ${req.query.age} years old.`
+  );
+});
+
+app.listen(8000, () => {
+  console.log("I'm listening in port 8000");
 });
